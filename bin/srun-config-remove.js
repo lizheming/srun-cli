8d2af655995f28d96d1b10e1e466cebf6943353b
lizheming <secretlzm007@gmail.com>
2015-06-30 22:46:22 +0800

    fix unix line_end bug

---
+++ bin/srun-config-remove.js
@@ -0,0 +1,16 @@
+var program = require("commander"),
+    fs = require("fs"),
+    path = require("path");
+
+program
+    .action(function(name) {
+        if( !name ) return false;
+
+        var config = require("../config.json");
+        delete config[name];
+        fs.writeFile(path.resolve(__dirname,"../config.json"), JSON.stringify(config, null, "\t"), function(err) {
+            console.log( err ? err : "删除成功" )
+        })
+    })
+    .parse(process.argv);
+
